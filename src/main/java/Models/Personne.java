e304c3b155a6656928200393cd39bfecf673b800
cyrinee <kmlsirine@gmail.com>
2019-01-10 15:16:45 +0100

    hello life

---
+++ src/main/java/Models/Personne.java
@@ -0,0 +1,69 @@
+package Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+
+public class Personne {
+
+	@Id
+	@GeneratedValue
+	private Long id_user;
+	private String nom;
+	private String prenom;
+	private int age;
+	private String mdp;
+	private String pays;
+	private String email;
+	public Long getId_user() {
+		return id_user;
+	}
+	public void setId_user(Long id_user) {
+		this.id_user = id_user;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getMdp() {
+		return mdp;
+	}
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	public String getPays() {
+		return pays;
+	}
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+	
+	
+	
+	
+
+}
