9317f15920347214322550352dd6fb9778f3e819
grimouche <grmtalice@gmail.com>
2019-01-10 17:56:55 +0100

    Update greetingcontroller.java

--- src/main/java/src/main/java/controller/greetingcontroller.java
+++ src/main/java/src/main/java/controller/greetingcontroller.java
@@ -1,16 +1,28 @@
 package src.main.java.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
-public class greetingcontroller {
+public class greetingcontroller implements ErrorController {
 	  @GetMapping("/greeting")
 	    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 	        model.addAttribute("name", name);
 	        return "greeting";
 
-	
+	  }
+	  
+	  private static final String PATH = "/error";
 
-}}
+	    @RequestMapping(value = PATH)
+	    public String error() {
+	        return "Error handling";
+	    }
+
+	    public String getErrorPath() {
+	        return PATH;
+	    }  
+}
